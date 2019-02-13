a27a0c89f8f39fa884a0dd82097f78598215db90
ccs <chaichuanshi@sunlands.com>
2019-02-13 15:32:41 +0800

    增加限制

--- src/main/java/cn/ccs/demo/service/binance/BinanceService.java
+++ src/main/java/cn/ccs/demo/service/binance/BinanceService.java
@@ -17,7 +17,7 @@ public class BinanceService {
 
     public List<BinanceEntity> find24hr(String exchange){
         Query query = new Query();
-        query.limit(20);
+        query.limit(3);
         return mongoTemplate.find(query,BinanceEntity.class);
     }
 }
