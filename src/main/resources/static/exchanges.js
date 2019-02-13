d98ecc7d3558ccfb9f0cfaba336b81d107203082
ccs <chaichuanshi@sunlands.com>
2019-02-13 16:37:25 +0800

    增加限制

--- src/main/resources/static/exchanges.js
+++ src/main/resources/static/exchanges.js
@@ -1,12 +1,12 @@
 $(function() {// 初始化内容
-    alert("aaaa");
+    //alert("aaaa");
     $.ajax({
         type: 'POST',
         url: "/coin/24hr",
         //data: data,
         success: function (data) {
             let innerData = data.data;
-            for(let i=1;i<innerData.length;i++){
+            for(let i=0;i<innerData.length;i++){
                 var content=$("#initTable").html()+createTr(i,innerData[i]);
                 $("#initTable").html(content);
             }
@@ -21,6 +21,8 @@ function createTr(num,obj){
     line+="<td>"+obj.symbol+"</td>"
     line+="<td>"+obj.symbol+"</td>"
     line+="<td>"+obj.symbol+"</td>"
+    line+="<td>"+obj.weightedAvgPrice+"</td>"
     line+="<td>"+obj.count+"</td>"
     line+="</tr>";
+    return line;
 }
